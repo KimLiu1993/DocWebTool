1696f056c699ce1b2b6f36519c85717e1373dd49
Jeffrey Yu <yuzc9@outlook.com>
2017-10-16 10:54:48 +0800

    First commit.

---
+++ main.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+# -*- Coding: UTF-8 -*-
+#------------------------------------
+#--Author:        Jeffrey Yu
+#--CreationDate:  2017/10/16 10:53
+#--RevisedDate:   
+#------------------------------------
+
