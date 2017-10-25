1316ac54b5a83d8b562b709e51c74887e3f9d664
Jeffrey Yu <yuzc9@outlook.com>
2017-10-25 15:11:54 +0800

    端口写在common里面

--- main.py
+++ main.py
@@ -233,4 +233,4 @@ def api_import_doc_mapping():
 
 if __name__ == '__main__':
     # app.debug = True
-    app.run(host='0.0.0.0', port=80, threaded=True)
+    app.run(host='0.0.0.0', port=common.domain.split(':')[1], threaded=True)
