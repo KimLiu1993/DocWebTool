4c4a77984a34a418218a2c0af2ccfe165de50927
sharol6 <32829335+sharol6@users.noreply.github.com>
2018-04-04 09:32:30 +0800

    1 在VAsubaccountcompare页面增加EMEA fundname对比功能 2，修改，main.py  VASubaccountCompare.py   VASubaccountCompare.html 3,增加GetLegalNameFromDocid.sql

--- VASubaccountCompare.py
+++ VASubaccountCompare.py
@@ -3,7 +3,7 @@
 #------------------------------------
 #--Author:        Jeffrey Yu
 #--CreationDate:  2018/1/25 9:54
-#--RevisedDate:   2018/2/7
+#--RevisedDate:   2018/4/3
 #------------------------------------
 
 import difflib
@@ -11,6 +11,20 @@ import datetime
 import pyodbc
 import pandas as pd
 import common
+
+
+# 获取sql
+with open(common.sql_path + 'GetLegalNameFromDocid.sql', 'r', encoding='UTF-8') as rr:
+    sql_code = rr.read()
+
+
+# 获取docmapping的legal name，返回list
+def get_mapfundlist(connection, code, docid):
+    cursor = connection.cursor()
+    result = cursor.execute(code %(docid)).fetchall()
+    fund_list = [row[0] for row in result]
+    cursor.close()
+    return fund_list
 
 
 def get_doc_mapping(connection, docid):
@@ -144,8 +158,63 @@ def run(docid, fundname_string):
         return '数据库正在同步当中，暂时无法使用，请稍后再试。'
     except Exception as e:
         return str(e)
-    
-    
+
+
+# 当selecttype == EMEA时，跑的函数  
+def run_mappingcompare(docid, fundname_string):
+    try:
+        # 格式化输入的字符串
+        docfund_list = []
+        if '\n' in fundname_string:
+            fund = fundname_string.split('\n')
+            for line in fund:
+                if len(line) > 0:
+                    line = line.strip()
+                    line = line.rstrip('\r')
+                    docfund_list.append(line)
+        else:
+            docfund_list.append(fundname_string)
+        docfund_list = sorted(set(docfund_list))
+
+        # get doc mapping legal name
+        connection = pyodbc.connect(common.connection_string_multithread)
+        mapfund_list = get_mapfundlist(connection, sql_code, docid)
+
+        total_result = []
+        ratio_result = []
+
+        # 从两两对比的ratio中取出ratio最高的，其余不要
+        ratio_list = [(mapfund, docfund, compare(mapfund, docfund)) for mapfund in mapfund_list for docfund in docfund_list]
+        pd_result = pd.DataFrame(ratio_list, columns=['MapFund', 'DocFund', 'Ratio']).sort_values(by='Ratio', ascending=False)
+        while pd_result.shape[0]>0:
+            temp_target = tuple(pd_result.head(1).values.tolist()[0])
+            ratio_result.append(temp_target)
+            if pd_result.shape[0]>0:
+                pd_result = pd_result[pd_result['MapFund'] != temp_target[0]]
+            if pd_result.shape[0]>0:
+                pd_result = pd_result[pd_result['DocFund'] != temp_target[1]]
+
+        # 处理剩余的没有对比的部分，并全并
+        if len(mapfund_list) >= len(docfund_list):
+            temp_list = [i for i in mapfund_list if i not in [items[0] for items in ratio_result]]
+            temp_result = [(i, '', '0') for i in temp_list]
+            total_result = total_result + ratio_result + temp_result
+        else:
+            temp_list = [i for i in docfund_list if i not in [items[1] for items in ratio_result]]
+            temp_result = [('', i, '0') for i in temp_list]
+            total_result = total_result + ratio_result + temp_result
+
+        # excel输出
+        pd_total_result = pd.DataFrame.from_records(total_result, columns=['MapFund', 'DocFund','Ratio'])
+        excel_name = 'MappingCompare-' + str(docid) + '-' + datetime.datetime.now().strftime('%Y%m%d%H%M%S') + '.xlsx'
+        pd_total_result.to_excel(common.temp_path + excel_name, encoding='UTF-8', index=False)
+        connection.close()
+
+        return excel_name
+    except pyodbc.Error:
+        return '数据库正在同步当中，暂时无法使用，请稍后再试。'
+    except Exception as e:
+        return str(e)  
 
 # docid = 132735313
 # fundname_string = 'ALPS Variable Investment Trust - ALPS/Alerian Energy Infrastructure Portfolio: Class III\nALPS Variable Investment Trust - ALPS/Red Rocks Listed Private Equity Portfolio: Class III'
